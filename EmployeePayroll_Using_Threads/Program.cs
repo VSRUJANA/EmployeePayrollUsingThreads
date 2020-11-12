fb89f381296565d1f0bc208a412172af4dafb515
VSRUJANA <srujana.valavala@gmail.com>
2020-11-13 01:23:47 +0530

    Add project files.

---
+++ EmployeePayroll_Using_Threads/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayroll_Using_Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
